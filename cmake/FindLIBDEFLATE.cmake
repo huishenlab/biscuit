83c750a47c6f640828d789206a0e6c454c4a3bc0
Jacob Morrison <Jacob.Morrison@vai.org>
2025-04-29 14:31:33 -0400

    consistent formatting and comments

--- cmake/FindLIBDEFLATE.cmake
+++ cmake/FindLIBDEFLATE.cmake
@@ -1,28 +1,33 @@
+# Search for libdeflate using pkg-config
 if(UNIX)
     find_package(PkgConfig QUIET)
-    # pkg-config support added in libdeflate v1.9
     pkg_check_modules(_LIBDEFLATE QUIET libdeflate)
 else()
-    message(FATAL_ERROR "BISCUIT only works on Unix systems")
+    message(FATAL_ERROR "BISCUIT currently only works on Unix systems")
 endif()
 
+# Get include directory and shared library path
 find_path(LIBDEFLATE_INCLUDE_DIR
     NAMES libdeflate.h
-    HINTS ${_LIBDEFLATE_INCLUDEDIR})
+    HINTS ${_LIBDEFLATE_INCLUDEDIR}
+    )
 find_library(LIBDEFLATE_LIBRARY
     NAMES deflate
-    HINTS ${_LIBDEFLATE_LIBDIR})
+    HINTS ${_LIBDEFLATE_LIBDIR}
+    )
 
+# Set variables for use in CMakeLists files
 set(LIBDEFLATE_INCLUDE_DIRS ${LIBDEFLATE_INCLUDE_DIR})
 set(LIBDEFLATE_LIBRARIES ${LIBDEFLATE_LIBRARY})
 
+# Pull version from pkg-config if possible, otherwise pull from libdeflate header
 if(_LIBDEFLATE_VERSION)
-        set(LIBDEFLATE_VERSION ${_LIBDEFLATE_VERSION})
+    set(LIBDEFLATE_VERSION ${_LIBDEFLATE_VERSION})
 elseif(LIBDEFLATE_INCLUDE_DIR)
-        file(STRINGS "${LIBDEFLATE_INCLUDE_DIR}/libdeflate.h" LIBDEFLATE_VERSION_STR
-                        REGEX "^#define[\t ]+LIBDEFLATE_VERSION_STRING[\t ]+\"[^\"]+\"")
-        if(LIBDEFLATE_VERSION_STR MATCHES "\"([^\"]+)\"")
-                set(LIBDEFLATE_VERSION "${CMAKE_MATCH_1}")
+    file(STRINGS "${LIBDEFLATE_INCLUDE_DIR}/libdeflate.h" LIBDEFLATE_VERSION_STR
+        REGEX "^#define[\t ]+LIBDEFLATE_VERSION_STRING[\t ]+\"[^\"]+\"")
+    if(LIBDEFLATE_VERSION_STR MATCHES "\"([^\"]+)\"")
+        set(LIBDEFLATE_VERSION "${CMAKE_MATCH_1}")
     endif()
 endif()
 
@@ -30,8 +35,8 @@ include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(LIBDEFLATE
     REQUIRED_VARS
-                LIBDEFLATE_INCLUDE_DIR
-                LIBDEFLATE_LIBRARY
+        LIBDEFLATE_INCLUDE_DIR
+        LIBDEFLATE_LIBRARY
     VERSION_VAR LIBDEFLATE_VERSION)
 
 mark_as_advanced(LIBDEFLATE_INCLUDE_DIR LIBDEFLATE_LIBRARY)
