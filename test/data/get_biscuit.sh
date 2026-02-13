b0f8cc838a60b68edaa6a083a42fa5302607edd7
Jacob Morrison <Jacob.Morrison@vai.org>
2026-02-13 10:38:45 -0500

    no need for biscuit download in testing

--- test/data/get_biscuit.sh
+++
@@ -1,8 +0,0 @@
-# Retrieve biscuit
-git clone git@github.com:huishenlab/biscuit.git biscuit_master
-
-# Move to biscuit directory and build
-cd biscuit_master
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=../ ../
-make install
